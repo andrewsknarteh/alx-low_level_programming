c1db29106e7e53c09f4cf3a204c246dda659fee4
andrewsknarteh <andrewsknarteh@gmail.com>
2022-09-08 22:00:06 -0700

    Print with no mmistake

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints exact text
+ *
+ * Return: Always 0 (A success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
