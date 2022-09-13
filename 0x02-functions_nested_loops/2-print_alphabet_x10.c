110041fc5158c6760ff9f44216cc31a30f28aad2
andrewsknarteh <andrewsknarteh@gmail.com>
2022-09-13 15:49:46 -0700

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,9 @@
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
- * Return: 0
+ * Return: Always 0
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	char c, i;
 	for (i = 0; i <= 9; i++)
