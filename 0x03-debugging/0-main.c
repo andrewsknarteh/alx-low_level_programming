e01fb9e076a70b4d9d774e0e59fd5ae201600464
andrewsknarteh <andrewsknarteh@gmail.com>
2022-09-14 11:04:53 -0700

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,13 @@
  * main - function prints positive or negative interger
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-		positive or nrgative(i);
+	positive or nrgative(i);
+	
 	return (0);
 }
