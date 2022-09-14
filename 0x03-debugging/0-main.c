419b02b2d6f4fb4a020df2f5d3152702988cbc2d
andrewsknarteh <andrewsknarteh@gmail.com>
2022-09-14 10:50:31 -0700

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - function prints positive or negative interger
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+		positive or nrgative(i);
+	return (0);
+}
