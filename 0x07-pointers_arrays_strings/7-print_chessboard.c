b58941ce852629bce043479fc41fb7180426d9bf
andrewsknarteh <andrewsknarteh@gmail.com>
2022-09-26 20:55:13 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,14 +9,13 @@
 void print_chessboard(char (*a)[8])
 {
 	int x = 0, y = 0;
-
+	
 	for (x = 0; x < 8; x++)
 	{
 		for (y = 0; y < 8; y++)
 		{
 			_putchar(a[x][y]);
 		}
-		
-		_putchar(10);
+		_putchar('\n');
 	}
 }
