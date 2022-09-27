d64d9cfd0565aa4a81ff87de6f02be2c2abec516
andrewsknarteh <andrewsknarteh@gmail.com>
2022-09-26 20:25:24 -0700

    assign value

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -12,3 +12,19 @@ void set_string(char **s, char *to)
 {
 	*s = to;
 }
+
+100-set_string.c
+
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to char
+ * @s: content to be copied
+ * @to: string
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
