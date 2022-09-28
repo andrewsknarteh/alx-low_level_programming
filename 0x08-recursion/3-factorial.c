fc7b86b3ae55e88ca1c0a7b83c6497881690c505
andrewsknarteh <andrewsknarteh@gmail.com>
2022-09-28 16:22:28 -0700

    frints factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,12 +6,14 @@
  *
  * Return: the factorial of the number
  */
-int factorial(int n)
+int factorial(int n);
 {
+	int next_factorial;
+
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	next_fatorial = factorial(n - 1);
+	next_factorial = factorial(n - 1);
 	return (n * next_factorial);
 }
