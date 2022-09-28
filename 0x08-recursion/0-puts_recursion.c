7a6a70536a597423af7a3718d8b98e5a53bcaf28
andrewsknarteh <andrewsknarteh@gmail.com>
2022-09-28 15:03:36 -0700

    prints recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		_putchar(s[0]);
-		_putchar(s + l);
-	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
