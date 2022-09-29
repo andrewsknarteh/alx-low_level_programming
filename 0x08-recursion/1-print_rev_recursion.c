10f9ff7a3c73bee99394f48e72fd5f4bcaa22a24
andrewsknarteh <andrewsknarteh@gmail.com>
2022-09-28 17:43:41 -0700

    print reversed string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.g"
+
+/**
+ * _print_rev_recursion - prints a reversed string
+ * @s: input
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
