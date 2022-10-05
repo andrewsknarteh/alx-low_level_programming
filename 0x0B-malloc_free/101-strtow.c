c5f5479341653ce51d4619d5e1078ebfad64d98c
andrewsknarteh <andrewsknarteh@gmail.com>
2022-10-05 04:45:31 -0700

    counts number of words in string

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -47,3 +47,54 @@ int number_words(char *str)
 	}
 	return (words);
 }
+
+/**
+ * strtow - splites a string into words
+ * @str: string to be spliteed
+ *
+ * Return: NULL or pointer to the array of splitted words
+ */
+
+char **strtow(char *str)
+{
+	char **split;
+	int i, j = 0, temp = 0, size = 0, words = num_words(str);
+
+	if (words == 0)
+		return (NULL);
+	split = (char **)malloc(sizeof(char *) * (words + 1));
+	if (split != NULL)
+	{
+		for (i = 0; i <= len(str) && words; i++)
+		{
+			if ((str[i] != ' ') && (str[i] != '\0'))
+				size++;
+			else if (((str[i] == ' ') || (str[i] == '\0')) && (str[i + 1] != ' '))
+			{
+				split[j] = (char *)malloc(sizeof(char) * size + 1);
+				if (split[j] != NULL)
+				{
+					while (temp < size)
+					{
+						split[j][temp] = str[(i - size) + temp];
+						temp++;
+					}
+					split[j][temp] = '\0';
+					size = temp = 0;
+					j++;
+				}
+				else
+				{
+					while (j-- >= 0)
+						free(split[j]);
+					free(split);
+					return (NULL);
+				}
+			}
+		}
+		spit[words] = NULL;
+		return (split);
+	}
+	else
+		return (NULL);
+}
