d7978fce8150259e24c5a8de7fbb7bec9fda73ca
andrewsknarteh <andrewsknarteh@gmail.com>
2022-10-14 09:53:00 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,12 +2,12 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - sums all of its parameters.
+ * sum_them_all - sums of all its parameters.
  * @n: number of parameters
  *
- * Return: sum of parameters
+ * Return: sum of all parameters
  */
-int sum_them_all(const unsingned int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,7 @@
 
 #include <stdarg.h>
 /**
- * struct -print types with corresponding print function
+ * struct print - print type with corresponding print function
  * @t: print type
  * @f: print function
  */
@@ -19,4 +19,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif /*_VARIADIC_FUNCTIONS_H_*/
+#endif /* _VARIADIC_FUNCTIONS_H_ */
